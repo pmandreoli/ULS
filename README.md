d6d65dcab737d214d2c04723e26586cd7e67e61a
pmandreoli <pietro.mandreoli@gmail.com>
2021-02-23 14:18:12 +0100

    fix typo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ULS
 useful little scripts
+
+#### m_ans_script.sh <ansible-version> 
+create a virtualenv and install inside a specific ansible version on CentOS7
--- m_ans_script.sh
+++ m_ans_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y epel-release python3 python3-devel gcc gcc-c++ vim wget;
+sudo yum install -y epel-release python3 python3-devel gcc gcc-c++ vim wget;
 pip3 install virtualenv --user;
 mkdir ansible$1;
 virtualenv ansible$1/;
