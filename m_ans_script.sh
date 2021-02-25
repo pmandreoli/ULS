3b5180e57afe3da785af2d296793471724a9f741
pmandreoli <pietro.mandreoli@gmail.com>
2021-02-25 10:40:30 +0100

    fix typo

--- m_ans_script.sh
+++ m_ans_script.sh
@@ -3,6 +3,6 @@ sudo yum install -y epel-release python3 python3-devel gcc gcc-c++ vim wget;
 pip3 install virtualenv --user;
 mkdir ansible$1;
 virtualenv ansible$1/;
-. ansible2.3/bin/activate;
+. ansible$1/bin/activate;
 pip install ansible==$1;
 
