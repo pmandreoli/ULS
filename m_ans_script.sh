b8fe49b88c17fbbb5cd0aa58f28f8e5c4912c3bb
pmandreoli <pietro.mandreoli@gmail.com>
2021-03-04 12:41:56 +0000

    add ubuntu

--- m_ans_script.sh
+++ m_ans_script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+distro=$(lsb_release -is) ;
+
+if [ "$distro" == "Ubuntu" ]
+then
+sudo apt install -y python3 python3-pip build-essential vim wget;
+else
 sudo yum install -y epel-release python3 python3-devel gcc gcc-c++ vim wget;
+fi
+
 pip3 install virtualenv --user;
 mkdir ansible$1;
 virtualenv ansible$1/;
